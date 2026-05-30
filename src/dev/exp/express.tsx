9c6a6302394b90e4ea0d780162c287b701277565
AGsalem <agsalem2025@outlook.com>
2026-05-30 09:20:28 +0300

    add table

--- src/dev/exp/express.tsx
+++ src/dev/exp/express.tsx
@@ -1,9 +1,19 @@
-export default function Expers(){
-    return(
+export default function Expers() {
+    return (
         <>
-        <div className="">
-            <h1>ex</h1>
-        </div>
+            <div className="bg-[#3639cebd] p-10 rounded-3xl">
+                <table>
+                    <thead></thead>
+                    <tr className="gap-2">
+                        <th>Experience </th>
+                        <th>Experience Duration </th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td> </td>
+                    </tr>
+                </table>
+            </div>
         </>
     )
 }
